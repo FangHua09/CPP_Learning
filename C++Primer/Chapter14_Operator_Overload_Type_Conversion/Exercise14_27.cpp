aea2f84bc3d3baac460b37fd38aef43e455c99c3
CamelliaByes <13075250778@163.com>
2026-09-26 18:49:40 +0800

    Adds increment, decrement, and pointer arithmetic

--- C++Primer/Chapter14_Operator_Overload_Type_Conversion/Exercise14_27.cpp
+++ C++Primer/Chapter14_Operator_Overload_Type_Conversion/Exercise14_27.cpp
@@ -1,4 +1,84 @@
 ﻿/*
- *
+ *知识点：++ -- 前置 / 后置；迭代器指针算术；const 成员函数语义
+ 规则：
+ 前置 ++/--：无参数，返回*this引用，成员函数，修改对象。
+ 后置 ++/--：int 哑元参数，返回原值副本，不返回引用。
+ 迭代器加法 iter + n、减法 iter - n：非成员，返回新 StrBlobPtr；iter1 - iter2 返回两个迭代器的下标差。
+ ++、-- 会修改迭代器本身，不能是 const 成员函数。
+ 练习 14.27：StrBlobPtr 添加递增、递减运算符
  */
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <ios>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <array>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <numeric>
+#include <bitset>
+#include <climits>
+#include <functional>
+#include <iterator>
+#include <memory>
+using namespace std;
+
+class StrBlobPtr
+{
+public:
+    // 前置 ++
+    StrBlobPtr &operator++();
+    // 后置 ++
+    StrBlobPtr operator++(int);
+    // 前置 --
+    StrBlobPtr &operator--();
+    // 后置 --
+    StrBlobPtr operator--(int);
+
+    // ...原有其他成员
+private:
+    weak_ptr<vector<string>> wptr;
+    size_t curr;
+    shared_ptr<vector<string>> check(size_t i, const string &msg) const; // 检查给定下标有没有超过范围
+};
+// 前置++
+StrBlobPtr &StrBlobPtr::operator++()
+{
+    check(curr, "越界");
+    ++curr;
+    return *this;
+}
+// 后置++
+StrBlobPtr StrBlobPtr::operator++(int)
+{
+    // 后置无需检查 交给前置
+    auto ret = *this;
+    ++*this;
+    return ret;
+}
+// 前置--
+StrBlobPtr &StrBlobPtr::operator--()
+{
+    --curr;
+    check(curr, "下溢");
+    return *this;
+}
+// 后置--
+StrBlobPtr StrBlobPtr::operator--(int)
+{
+    auto ret = *this;
+    --*this;
+    return ret;
+}
--- C++Primer/Chapter14_Operator_Overload_Type_Conversion/Exercise14_28.cpp
+++ C++Primer/Chapter14_Operator_Overload_Type_Conversion/Exercise14_28.cpp
@@ -1,4 +1,82 @@
 ﻿/*
- *
+ *练习 14.28：指针算术：+、-
+ 语义：iter + n：迭代器向后移动 n 个位置；iter - n向前移动 n；iter1 - iter2求下标差值。
  */
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <ios>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <array>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <numeric>
+#include <bitset>
+#include <climits>
+#include <functional>
+#include <iterator>
+#include <memory>
+using namespace std;
+
+class StrBlobPtr
+{
+    friend StrBlobPtr operator+(const StrBlobPtr &lhs, size_t n);
+    friend StrBlobPtr operator-(const StrBlobPtr &lhs, size_t n);
+    friend ptrdiff_t operator-(const StrBlobPtr &lhs, const StrBlobPtr &rhs);
+
+public:
+    // ...原有代码
+    string &operator[](size_t offset);
+    const string &operator[](size_t offset) const;
+
+private:
+    weak_ptr<vector<string>> wptr;
+    size_t curr;
+    shared_ptr<vector<string>> check(size_t i, const string &msg) const;
+};
+
+string &StrBlobPtr::operator[](size_t offset)
+{
+    auto p = check(curr + offset, "越界");
+    return (*p)[curr + offset];
+}
+
+const string &StrBlobPtr::operator[](size_t offset) const
+{
+    auto p = check(curr + offset, "越界");
+    return (*p)[curr + offset];
+}
+
+StrBlobPtr operator+(const StrBlobPtr &lhs, size_t n)
+{
+    auto temp = lhs;
+    temp.check(temp.curr + n, "越界"); // 先check再加
+    temp.curr += n;
+    return temp;
+}
+StrBlobPtr operator-(const StrBlobPtr &lhs, size_t n)
+{
+    auto temp = lhs;
+    temp.curr -= n; // 先减再check
+    temp.check(temp.curr - n, "越界");
+    return temp;
+}
+ptrdiff_t operator-(const StrBlobPtr &lhs, const StrBlobPtr &rhs)
+{
+    auto l = lhs.wptr.lock(), r = rhs.wptr.lock();
+    if (!l || !r || l != r)
+        throw runtime_error("iterators point to different containers");
+    return r.get() - l.get();
+}
